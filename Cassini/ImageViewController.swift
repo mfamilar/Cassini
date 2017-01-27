060c036623da89a9f4429bbaa5a3a9cec5921f06
daftMarc <marc.familari@gmail.com>
2017-01-27 15:10:46 +0100

    initial commit

---
+++ Cassini/ImageViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by Marc FAMILARI on 1/27/17.
+//  Copyright © 2017 Marc FAMILARI. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    var imageURL: NSURL? {
+        didSet {
+            image = nil
+            fetchImage()
+        }
+    }
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            if let imageData = NSData(contentsOf: url as URL) {
+                image = UIImage(data: imageData as Data)
+            }
+        }
+    }
+    
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet {
+            scrollView.contentSize = imageView.frame.size
+        }
+    }
+    
+    private var imageView = UIImageView()
+    
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            scrollView?.contentSize = imageView.frame.size
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.addSubview(imageView)
+        imageURL = NSURL(string: "http://boingboing.net/features/cassini/cassini6.jpg")
+    }
+}
