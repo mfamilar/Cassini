da32189b63e4ac33cdf86bd5b9eacbb847836ba6
daftMarc <marc.familari@gmail.com>
2017-01-27 17:16:09 +0100

    begin of lesson 8

---
+++ Cassini/CassiniViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CassiniViewController.swift
+//  Cassini
+//
+//  Created by Marc FAMILARI on 1/27/17.
+//  Copyright © 2017 Marc FAMILARI. All rights reserved.
+//
+
+import UIKit
+
+class CassiniViewController: UIViewController {
+
+    private struct Storyboard {
+        static let ShowImageSegue = "Show Image"
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+}
